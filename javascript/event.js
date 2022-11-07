c8f2dce4fadfd3dc6aba3882bc90ece4de123227
alex742022 <alexis.aquino.lumbera7@gmail.com>
2022-11-07 17:40:36 +0800

    added style and mediaquery

--- javascript/event.js
+++ javascript/event.js
@@ -3,10 +3,11 @@
 const menuBar = document.querySelector(".menu-icon")
 const navBarContainer = document.querySelector(".navbar-container")
 const xIcon = document.querySelector(".x-icon")
-const searchIcon = document.querySelector(".search-icon-toggle")
+const searchIcon = document.querySelector(".search-container")
 const navBarSearchContainer = document.querySelector(".navbar-search-container")
 const searchCancelToggle =document.querySelector(".search-cancel-toggle")
 const inputContainer = document.querySelector(".input-container")
+const headerOne = document.querySelector(".header-1")
 
 menuBar.addEventListener("click", () => {
     navBarContainer.style.right = "0"
@@ -17,6 +18,7 @@ xIcon.addEventListener("click", () => {
 searchIcon.addEventListener("click", () => {
     navBarSearchContainer.style.top = "0"
     inputContainer.style.width = "100%"
+    inputContainer.style.maxWidth = "650px"
     searchCancelToggle.style.transform = "scale(1)"
 })
 searchCancelToggle.addEventListener("click", () => {
@@ -57,3 +59,29 @@ const arrowRight = document.querySelector(".arrow-right")
 //             console.log(okay)
 // 		}
 // })
+
+// Section-5 list item function
+
+const listOne = document.querySelector(".list-one")
+const listTwo = document.querySelector(".list-two")
+const listThree = document.querySelector(".list-three")
+const listFour = document.querySelector(".list-four")
+const eachListContainerOne = document.querySelector(".list-content-one")
+const eachListContainerTwo = document.querySelector(".list-content-two")
+const eachListContainerThree = document.querySelector(".list-content-three")
+const eachListContainerFour = document.querySelector(".list-content-four")
+const eachListContainer = document.querySelector(".each-list-container")
+
+listOne.addEventListener("click", () => {
+    eachListContainerOne.classList.toggle("each-list-container-toggle-one")
+})  
+listTwo.addEventListener("click", () => {
+    eachListContainerTwo.classList.toggle("each-list-container-toggle-one")
+})
+listThree.addEventListener("click", () => {
+    eachListContainerThree.classList.toggle("each-list-container-toggle-three")
+})
+listFour.addEventListener("click", () => {
+    eachListContainerFour.classList.toggle("each-list-container-toggle-one")
+})
+
